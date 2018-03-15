9fb0953acaf6ec1504b071a8e12ca9b7c431aa58
JULIE WANJA MUGIRA <julie.mugira@andela.com>
2018-03-15 13:02:04 +0300

    add api routes

--- readme.md
+++ readme.md
@@ -2,3 +2,10 @@
 ## About weConnect
 
 weConnect is a simple business listing app with a laravel backe-end and react front-end
+
+# API endpoints 
+GET /business/: Retrieve all businesses.
+GET /business/{id}: Retrieve the business that matches the id.
+POST /business: Create a new business and insert it into the database.
+PUT /business/{id} : Update an existing business that matches the id.
+DELETE /business/{id}: Delete the product business the given id.
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,23 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('business', function () {
+    return response(['business 1', 'business 2'], 200);
+});
+
+Route::get('business/{id}', function ($businessId) {
+    return repsonse()->json(['businessId' => "{businessId}"], 200);
+});
+
+Route::post('business', function () {
+    return response()->json(['message' => "Business created successfully!"], 201);
+});
+
+Route::put('business', function () {
+    return response()->json(['message' => "Business details updated!"], 200);
+});
+
+Route::delete('business', function () {
+    return response()->json(null, 204);
+});
